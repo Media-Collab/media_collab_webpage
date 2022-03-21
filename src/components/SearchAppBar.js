8cd2d9c7a64081f429303b135b0a5b4669fac596
Gregson Murcia Castro <56243758+GregoryInnovo@users.noreply.github.com>
2022-03-20 23:10:55 -0500

    [ADD] Main content to the page

---
+++ src/components/SearchAppBar.js
@@ -0,0 +1,141 @@
+import * as React from 'react';
+import Image from 'next/image';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+import mediacollabLogo from "../../public/mediacollab_logo.svg"
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+const pages = ['Home', 'Projects', 'Blog'];
+
+export default function SearchAppBar() {
+
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{bgcolor: 'primary.main'}}>
+        <Toolbar>
+          <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+          <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Media Collab
+          </Typography>
+            
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Typography from '@mui/material/Typography';
+import SearchAppBar from '../components/SearchAppBar';
+import mediacollabLogo from "../../public/mediacollab_logo.svg";
+import Box from '@mui/material/Box';
 
 export default function Home() {
   return (
@@ -10,23 +14,65 @@ export default function Home() {
         <meta name="description" content="Media Collab official webpage" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-
+  
+      <SearchAppBar />
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://www.mediacollab.co">Media Collab</a>
         </h1>
+        <Image
+          src={mediacollabLogo}
+          alt="Media Collab Logo"
+          width="200%"
+          height="200%"
+        />
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>👌</code>
+          Student Group{' '} 
+          <code className={styles.code}>aimed at students who seek to put into practice their knowledge in programming, art, design, and audio, in which experiences are shared and collaborative teams are worked on to create interactive features and develop applications to solve problems. It is a multidisciplinary space that aims to encourage members to participate in projects, calls and other events to create a personal portfolio, develop experience and acquire knowledge for professional projects.</code>
         </p>
 
+        <div sx={{
+                display: { xs: 'block', md: 'none' }
+              }}>
+          <h1>Roles you can develop</h1>  
+          <p className={styles.info}>Programmer</p>
+          <p className={styles.info}>Designer</p>
+          <p className={styles.info}>Multimedia content</p>
+        </div>
+
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/" className={styles.card}>
+            <h2>Members &rarr;</h2>
+            <p>Meet the people behind the scenes.</p>
+          </a>
+          <a href="https://github.com/Media-Collab" className={styles.card}>
+            <h2>Projects &rarr;</h2>
+            <p>Explore all the projects that the group developed.</p>
+          </a>
+          <a href="/" className={styles.card}>
+            <h2>Blog &rarr;</h2>
+            <p>Discover about some tips, recommendations and experiences ours members.</p>
           </a>
         </div>
+        <h1>Join our discord✌</h1> 
+        <div sx={{
+                display: { xs: 'block', md: 'none' }
+              }}>
+          <h2 className={styles.info}><a href="https://discord.gg/jatmyDUb3h">Click here!</a></h2>
+        </div>
+  
+        <h1>Our schedule📅</h1> 
+        <Box  sx={{
+                display: 'grid', gridTemplateColumns: 'repeat(1, 1fr)'
+              }}>
+          <h3 className={styles.info}>Tuesday: <a>Dev front/back end</a> 12.30p.m.</h3>
+          <h3 className={styles.info}>Wednesday: <a>VideoGames</a> 1.00p.m.</h3>
+          <h3 className={styles.info}>Thursday: <a>Mobile</a> 12.30p.m.</h3>
+          <h3 className={styles.info}>In <a>Infolab</a> UAO</h3>
+        </Box>
+        
+            
       </main>
 
       <footer className={styles.footer}>
