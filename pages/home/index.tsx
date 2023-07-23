00e1218eb5ea2b51de6e4cc0fab46dda142a2e49
GregoryInnovo <gregoryinnovo@gmail.com>
2023-07-23 17:30:31 -0500

    [ADD] initial files

---
+++ pages/home/index.tsx
@@ -0,0 +1,34 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { Stack, Text, Heading } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <Stack spacing={3}>
+      <Heading>Hello World</Heading>
+      <Text fontSize="6xl">(6xl) In love with React & Next</Text>
+      <Text fontSize="5xl">(5xl) In love with React & Next</Text>
+      <Text fontSize="4xl">(4xl) In love with React & Next</Text>
+      <Text fontSize="3xl">(3xl) In love with React & Next</Text>
+      <Text fontSize="2xl">(2xl) In love with React & Next</Text>
+      <Text fontSize="xl">(xl) In love with React & Next</Text>
+      <Text fontSize="lg">(lg) In love with React & Next</Text>
+      <Text fontSize="md">(md) In love with React & Next</Text>
+      <Text fontSize="sm">(sm) In love with React & Next</Text>
+      <Text fontSize="xs">(xs) In love with React & Next</Text>
+    </Stack>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: false,
+  };
+};
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  return {
+    props: {},
+  };
+};
+
+export default Home;
